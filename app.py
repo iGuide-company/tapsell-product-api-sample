56c57760a03b2cf2b918ba339cd8461e67f9a5d7
omidima <o.h.hadidy@gmail.com>
2023-09-01 11:14:49 +0330

    Add template

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 import json
 from fastapi import FastAPI
 import requests
+from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
+
+@app.get("/",response_class=HTMLResponse)
+def index():
+    return f'''
+<html>
+    <head>
+        <link rel="stylesheet" href="https://iguide-backend.darkube.app/public/08ea9206-1662-45d9-8208-5a61b246c26d/assets/index-f884a369.css" />
+        <title>Tapsell sample use iguide</title>
+    </head>
+    <body>
+        <div style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:300px;text-align:center;"><h1>Hi Dear.</h1> <p>Wellcome to iGuide for use this tools for answer to your question about tapsell open chatbox.</p></div>
+        <div id="shoper-root"></div>
+        <script src="https://iguide-backend.darkube.app/public/08ea9206-1662-45d9-8208-5a61b246c26d/assets/index-93a07def.js"></script>
+    </body>
+</html>
+'''
 
 @app.get("/products")
 def getData():
@@ -16,7 +34,7 @@ def getData():
     result = []
     for item in response.json():
         result.append({
-            "id": item['id'],
+            "id": str(item['id']),
             "title": item['title']['rendered'],
             "attr": [
                 {
